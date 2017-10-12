21f2b94831d95d3cfc78fe70d0bf3dcd7b46bb6b
Rei Lin <temisrei@gmail.com>
2017-10-12 13:46:43 +0800

    practice If... Else

---
+++ Level.java
@@ -0,0 +1,22 @@
+public class Level {
+  public static void main(String[] args) {
+    int score = 88;
+    char level;
+    if(score >= 90) {
+      level = 'A';
+    }
+    else if(score >= 80 && score < 90) {
+      level = 'B';
+    }
+    else if(score >= 70 && score < 80) {
+      level = 'C';
+    }
+    else if(score >= 60 && score < 70) {
+      level = 'D';
+    }
+    else {
+      level = 'E';
+    }
+    System.out.printf("得分 %d 等級判定為：%c%n", score, level);
+  }
+}
